8f5527a68c51c3a85693f19cae7e9bf8cfd7c64f
mariagutierrezm <gutierrez.mari90@gmail.com>
2020-11-19 15:42:46 +0000

    added image, fixed main page, added activity

--- app/src/main/java/com/example/library/MainActivity.java
+++ app/src/main/java/com/example/library/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.library;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnAllBooks, btnRead, btnWant, btnCurrent, btnFavourite, btnAbout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+
+    }
+
+    private void initViews() {
+        btnAllBooks = findViewById(R.id.btnAllBooks);
+        btnRead = findViewById(R.id.btnRead);
+        btnWant = findViewById(R.id.btnWishList);
+        btnCurrent = findViewById(R.id.btnReading);
+        btnFavourite = findViewById(R.id.btnFavorites);
+        btnAbout = findViewById(R.id.btnAbout);
     }
 }
